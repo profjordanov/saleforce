cba7f37bda7e355e6ee4eaa8f80ef9490c4d09f6
Jordan Jordanov <d_yugioh@abv.bg>
2019-08-29 21:47:24 +0300

    saleforce v1.0.2

--- backend/Saleforce.Common.Cqrs/Saleforce.Common.Cqrs/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Saleforce.Common.Cqrs
-{
-    public class Class1
-    {
-    }
-}
---
+++ backend/Saleforce.Common.Cqrs/Saleforce.Common.Cqrs/Core/IQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Saleforce.Common.Cqrs.Core
+{
+    public interface IQuery<out TResponse> : IRequest<TResponse>
+    {
+    }
+}
