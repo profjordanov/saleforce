e3222acf1eb14c8820d0be86321c169741fe6506
Jordan Jordanov <d_yugioh@abv.bg>
2019-08-30 22:23:42 +0300

    saleforce v1.0.4

---
+++ backend/Saleforce.Common.Cqrs/Saleforce.Common.Cqrs/Business/VoidBaseHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Optional;
+using Saleforce.Common.Cqrs.Core;
+
+namespace Saleforce.Common.Cqrs.Business
+{
+    public abstract class VoidBaseHandler<TCommand> : ICommandHandler<TCommand>
+        where TCommand : ICommand
+    {
+        public Task<Option<Unit, Error>> Handle(TCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
