d22ba7d09533157b653e8f1a0162cdc2a0fb2e23
Jordan Jordanov <d_yugioh@abv.bg>
2019-09-01 20:03:12 +0300

    saleforce v1.0.7

---
+++ backend/Saleforce.Customers.Api/Saleforce.Customers.Api/Hateoas/RegisterCustomerResources/RegisterCustomerResource.cs
@@ -0,0 +1,8 @@
+﻿using Saleforce.Common.Hateoas.Core;
+
+namespace Saleforce.Customers.Api.Hateoas.RegisterCustomerResources
+{
+    public class RegisterCustomerResource : Resource
+    {
+    }
+}
---
+++ backend/Saleforce.Customers.Api/Saleforce.Customers.Api/Hateoas/RegisterCustomerResources/RegisterCustomerResourcePolicy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RiskFirst.Hateoas;
+using Saleforce.Common.Hateoas;
+using Saleforce.Common.Hateoas.Core;
+
+namespace Saleforce.Customers.Api.Hateoas.RegisterCustomerResources
+{
+    public class RegisterCustomerResourcePolicy : IPolicy<RegisterCustomerResource>
+    {
+        public Action<LinksPolicyBuilder<RegisterCustomerResource>> PolicyConfiguration => builder =>
+        {
+            builder.RequireSelfLink();
+            //builder.RequireRoutedLink(LinkNames.Customers.GetAll, nameof())
+        };
+    }
+}
